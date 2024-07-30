9c9c82eb9b73fa617b7e41aad917cca10d05e468
213299298 <25sekhwelad@gmail.com>
2024-07-30 18:12:34 +0200

    Added Greetings Configuration

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddApplicationInsightsTelemetry();
+
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions
+{
+    ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+});
 
 var app = builder.Build();
 
